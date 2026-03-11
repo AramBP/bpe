f66e3babda83b38ec526c647856aa2393a2811be
Aram Brouwer <aramvanderpols05@gmail.com>
2026-03-11 16:24:59 +0100

    Simple test

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,17 @@
 #include "Tokenizer.h"
 
 int main() {
-
-
+    Tokenizer tokenizer;
+    std::string text = "aaabdaaabac";
+    tokenizer.bpeTrain(text, 3);
+    std::vector<tokenId_t> encoded = tokenizer.encode(text);
+    std::string decoded = tokenizer.decode(encoded);
+    std::cout << "Test text : " << text << std::endl
+              << "Encoded: [ ";
+    for (const tokenId_t& tok : encoded)
+        std::cout << tok << " ";
+    std::cout << " ]" << std::endl
+            << "Decoded : " << decoded << std::endl;
 
     return 0;
 }
